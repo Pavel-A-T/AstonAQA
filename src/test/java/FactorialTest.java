51d5e18eca6784b92230eeeb3712ebc6a96ebcb3
Pavel Titov <pavelt2022@gmail.com>
2024-03-18 22:20:11 +0300

    Исправил на assertTrue()

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -7,16 +7,15 @@ public class FactorialTest {
     @Test
     @DisplayName("Позитивный тест проверки расчета факториала")
     public void positiveTest() {
-        assertEquals(120, Factorial.factorial(5));
-        assertEquals(720, Factorial.factorial(6));
-        assertEquals(1, Factorial.factorial(0));
-        assertEquals(1, Factorial.factorial(1));
+        assertTrue(120 == Factorial.factorial(5));
+        assertTrue(720 == Factorial.factorial(6));
+        assertTrue(1 == Factorial.factorial(0));
+        assertTrue(1 == Factorial.factorial(1));
     }
 
     @Test
     @DisplayName("Негативный тест проверки расчета факториала")
     public void negativeTest() {
-        assertNotEquals(800, Factorial.factorial(5));
-
+        assertFalse(800 == Factorial.factorial(5));
     }
 }
